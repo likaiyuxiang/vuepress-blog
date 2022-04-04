abbaa6891d0983d6c4a4fa31136df96e1f0996bb
likaiyuxiang <591539878@qq.com>
2022-04-04 20:59:37 +0800

    第一次提交

---
+++ vuepress-blog/docs/baodian/high/sidebar.js
@@ -0,0 +1,11 @@
+module.exports =[ 
+   
+            {
+                  title:'高级宝典',
+                  collapsable: true,
+                  children:[
+                    '/baodian/high/notes/one',
+                  ]
+                }
+            	
+]
