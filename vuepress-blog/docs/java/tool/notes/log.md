e27eaef257d925ff44456f8455b931d57a309bea
likaiyuxiang <591539878@qq.com>
2022-06-30 00:56:19 +0800

    0

---
+++ vuepress-blog/docs/java/tool/notes/log.md
@@ -0,0 +1,13 @@
+---
+title: log
+date: 2022-06-30
+sidebar: 'auto'
+categories:
+ - frontEnd
+tags:
+ - springboot
+ - 日志
+---
+
+# Log
+
---
+++ vuepress-blog/docs/java/tool/sidebar.js
@@ -0,0 +1,12 @@
+module.exports =[ 
+   
+            {
+                  title:'日志',
+                  collapsable: true,
+                  children:[
+                    '/java/tool/notes/log',
+                  ]
+                },
+					 
+            	
+]
