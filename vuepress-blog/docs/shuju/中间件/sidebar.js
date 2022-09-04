06455886ed9f928aac84b6d2928e615a30934dcc
wugang11 <wugang11@meituan.com>
2022-09-04 10:59:34 +0800

    shuju

---
+++ vuepress-blog/docs/shuju/中间件/sidebar.js
@@ -0,0 +1,55 @@
+module.exports =[ 
+   
+            {
+                  title:'mysqll事务',
+                  collapsable: true,
+                  children:[
+                    '/shuju/one/notes/mysqll事务',
+                  ]
+                },
+					  			      {
+                  title:'mysql三种日志',
+                  collapsable: true,
+                  children:[
+                    '/shuju/one/notes/mysql三种日志',
+                  ]
+                },
+					  {
+                  title:'mysql查询优化',
+                  collapsable: true,
+                  children:[
+                    '/shuju/one/notes/mysql查询优化',
+                  ]
+                },
+					    {
+                  title:'mysql结构原理',
+                  collapsable: true,
+                  children:[
+                    '/shuju/one/notes/mysql结构原理',
+                  ]
+                },
+					    {
+                  title:'mysql死锁以及解决',
+                  collapsable: true,
+                  children:[
+                    '/shuju/one/notes/mysql死锁以及解决',
+                  ]
+                },
+					    {
+                  title:'mysql索引',
+                  collapsable: true,
+                  children:[
+                    '/shuju/one/notes/mysql索引',
+                  ]
+                },
+					    {
+                  title:'mysql锁',
+                  collapsable: true,
+                  children:[
+                    '/shuju/one/notes/mysql锁',
+					'/shuju/one/notes/mysql锁-2',
+                  ]
+                },
+
+				
+]
